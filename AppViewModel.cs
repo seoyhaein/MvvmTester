1a59f77522dcb48affa8de6048dd08b75fc46af7
Young Jun Seo <fighton.yj@gmail.com>
2022-12-21 14:16:18 +0900

    first commit

---
+++ AppViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmTester
+{
+
+    public class AppViewModel : ObservableObject
+    {
+        public AppViewModel() {}
+
+        private System.Drawing.Color myColor = System.Drawing.Color.Red;
+       
+        public System.Drawing.Color MyColor
+        {
+            get => myColor;
+            set => SetProperty(ref myColor, value);
+
+        }
+
+        private System.Windows.Media.Color myNewColor = System.Windows.Media.Colors.Lime;
+
+        public System.Windows.Media.Color MyNewColor 
+        {
+            get => myNewColor;
+            set => SetProperty(ref myNewColor, value);  
+        }
+
+    }
+}
---
+++ ObservableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MvvmTester
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool SetProperty<T>(ref T reference, T value, [CallerMemberName] in string propertyName = default!)
+        {
+            if (!Equals(reference, value))
+            {
+                reference = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+
+            return false;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] in string? propertyName = default)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
